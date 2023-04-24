02b97aae3a16f70382271fc41922c0951153e004
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-24 20:25:12 +0530

    [Pankaj] To compare equality of two line

--- LineComparisonComputationProg/Program.cs
+++ LineComparisonComputationProg/Program.cs
@@ -11,25 +11,47 @@ namespace LineComparisonComputationProg
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            //input
-            Console.Write("Enter the x-coordinate of the first point: ");
-            double x1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Enter the y-coordinate of the first point: ");
-            double y1 = double.Parse(Console.ReadLine());
+            // input for line 1
+            Console.Write("Enter the x-coordinate of the first point of line 1: ");
+            double x1_l1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Enter the x-coordinate of the second point: ");
-            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the y-coordinate of the first point of line 1: ");
+            double y1_l1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Enter the y-coordinate of the second point: ");
-            double y2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the x-coordinate of the second point of line 1: ");
+            double x2_l1 = double.Parse(Console.ReadLine());
 
-            // Calculate the length of the line using the distance formula
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.Write("Enter the y-coordinate of the second point of line 1: ");
+            double y2_l1 = double.Parse(Console.ReadLine());
 
-            // result 
-            Console.WriteLine("The length of the line is: {0}", length);
+            // input for line 2
+            Console.Write("Enter the x-coordinate of the first point of line 2: ");
+            double x1_l2 = double.Parse(Console.ReadLine());
 
+            Console.Write("Enter the y-coordinate of the first point of line 2: ");
+            double y1_l2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the x-coordinate of the second point of line 2: ");
+            double x2_l2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the y-coordinate of the second point of line 2: ");
+            double y2_l2 = double.Parse(Console.ReadLine());
+
+            // To alculate the length of line 1 and line 2 using the distance formula
+            double length_l1 = Math.Sqrt(Math.Pow(x2_l1 - x1_l1, 2) + Math.Pow(y2_l1 - y1_l1, 2));
+            double length_l2 = Math.Sqrt(Math.Pow(x2_l2 - x1_l2, 2) + Math.Pow(y2_l2 - y1_l2, 2));
+
+            // To compare the end points of line 1 and line 2
+            if ((x1_l1 == x1_l2 && y1_l1 == y1_l2 && x2_l1 == x2_l2 && y2_l1 == y2_l2) || (x1_l1 == x2_l2 && y1_l1 == y2_l2 && x2_l1 == x1_l2 && y2_l1 == y1_l2))
+            {
+                Console.WriteLine("Line 1 and Line 2 are equal");
+            }
+            else
+            {
+                Console.WriteLine("Line 1 and Line 2 are not equal");
+            }
         }
     }
 }
+
